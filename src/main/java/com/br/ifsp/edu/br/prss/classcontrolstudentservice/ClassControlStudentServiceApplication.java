0aa1ceebe3c22e005ea206b86964625860abe353
Gustavo <fernandes.guga@hotmail.com>
2021-01-05 00:35:48 -0300

    Iniciando projeto Spring

---
+++ src/main/java/com/br/ifsp/edu/br/prss/classcontrolstudentservice/ClassControlStudentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.br.ifsp.edu.br.prss.classcontrolstudentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassControlStudentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassControlStudentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/ifsp/edu/br/prss/classcontrolstudentservice/ClassControlStudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ifsp.edu.br.prss.classcontrolstudentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassControlStudentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
